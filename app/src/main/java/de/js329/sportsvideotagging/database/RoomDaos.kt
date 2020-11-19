a76fa28f95f5cbebd1f2ff8fc00c9d37fe7eb958
Jannik Schlemmer <js329@hdm-stuttgart.de>
2020-11-19 15:41:46 +0100

    implemented Room Database and DAOs for insertion data

--- app/src/main/java/de/js329/sportsvideotagging/database/RoomDaos.kt
+++ app/src/main/java/de/js329/sportsvideotagging/database/RoomDaos.kt
@@ -1,2 +1,68 @@
 package de.js329.sportsvideotagging.database
 
+import androidx.room.*
+import de.js329.sportsvideotagging.datamodels.*
+
+@Dao
+interface MatchDao {
+
+    @Insert
+    fun insertAll(vararg matches: Match)
+
+    @Update
+    fun updateAll(vararg matches: Match)
+
+    @Delete
+    fun delete(match: Match)
+}
+
+@Dao
+interface PlayerDao {
+    @Insert
+    fun insertAll(vararg players: Player): List<Long>
+
+    @Update
+    fun updateAll(vararg players: Player)
+
+    @Delete
+    fun delete(player: Player)
+}
+
+@Dao
+interface TeamDao {
+
+    @Query("SELECT * FROM Team")
+    fun getAll(): List<Team>
+
+    @Insert
+    fun insertAll(vararg teams: Team): List<Long>
+
+    @Update
+    fun updateAll(vararg teams: Team)
+
+    @Delete
+    fun delete(team: Team)
+}
+
+@Dao
+interface EventDao {
+
+    @Insert
+    fun insertAllEventAttributes(vararg attributes: EventAttribute): List<Long>
+
+    @Insert
+    fun insertAllEventTypes(vararg eventTypes: EventType): List<Long>
+
+    @Insert
+    fun insertAllMatchEvents(vararg matchEvents: MatchEvent): List<Long>
+}
+
+@Dao
+interface EventJoinDao {
+
+    @Insert
+    fun insertAllEventPlayerJoins(vararg eventPlayerJoin: MatchEventPlayer): List<Long>
+
+    @Insert
+    fun insertAllEventAttributeJoins(vararg eventAttributeJoin: MatchEventAttribute): List<Long>
+}
--- app/src/main/java/de/js329/sportsvideotagging/database/VideoTagDatabase.kt
+++ app/src/main/java/de/js329/sportsvideotagging/database/VideoTagDatabase.kt
@@ -2,9 +2,24 @@ package de.js329.sportsvideotagging.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import de.js329.sportsvideotagging.datamodels.*
 
-@Database(entities = [Match::class, Team::class, Player::class, EventAttribute::class, EventType::class, MatchEvent::class, MatchEventAttribute::class, MatchEventPlayer::class], version = 1)
+@Database(
+    entities = [
+        Match::class,
+        Team::class,
+        Player::class,
+        EventAttribute::class,
+        EventType::class,
+        MatchEvent::class,
+        MatchEventAttribute::class,
+        MatchEventPlayer::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(DateConverter::class)
 abstract class VideoTagDatabase: RoomDatabase() {
     abstract fun playerDao(): PlayerDao
     abstract fun teamDao(): TeamDao
